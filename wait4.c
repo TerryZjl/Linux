37e4ce82a8ec46ca6918a7baf5d407e7bc492bc2
long <1538068051@qq.com>
2017-12-09 05:14:43 -0800

    waitpid

---
+++ wait4.c
@@ -0,0 +1,51 @@
+/*************************************************************************
+	> File Name: wait1.c
+	> Author: ma6174
+	> Mail: ma6174@163.com 
+	> Created Time: Thu 07 Dec 2017 02:56:05 AM PST
+ ************************************************************************/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<unistd.h>
+#include<sys/wait.h>
+
+int main()
+{
+	pid_t pid = fork();
+	if(pid == -1){
+		perror("fork");
+		exit(1);
+	}
+	if(pid == 0){
+		int i = 0;
+		while(1){
+			if(++i == 3)break;
+			printf("child pid:%d\n",getpid());
+			sleep(1);
+		}
+		exit(2);
+	}
+	else{
+		int status;
+		printf("parent pid: %d \n",getpid());
+		//sleep(3);
+		pid_t w = waitpid(-1,&status,WNOHANG);
+
+		if(w == -1)	{
+			perror("wait");
+		}
+		else{
+		    printf("ret code w = %d\n",w);
+		}
+
+		if(WIFEXITED(status)){
+			printf("exit code = %d\n",WEXITSTATUS(status));
+		}else if(WIFSIGNALED(status)){
+			printf("SIG code = %d\n",WTERMSIG(status));
+		}
+	}
+	return 0;
+}
+
