a625d28baef8e5072c80d4484d1c38cc9e42e42a
long <1538068051@qq.com>
2018-01-16 00:20:31 -0800

    生产者消费者模型-共享内存和信号量实现

---
+++ shmfifo/shmfifo.c
@@ -0,0 +1,91 @@
+#include"shmfifo.h"
+
+union semun{
+	int val;
+};
+
+shmfifo_t *shmfifo_init(key_t key, int blksz, int blks)
+{
+	shmfifo_t *fifo = malloc(sizeof(shmfifo_t));
+	assert(fifo);
+	memset(fifo, 0, sizeof(shmfifo_t));
+
+	int size = sizeof(shmfifo_t)+ blksz*blks;
+	int shmid = shmget(key,0,0);
+	if(shmid == -1){//共享受内存不存在需要打开
+		shmid = shmget(key, size, IPC_CREAT|0644);
+		if(shmid == -1){
+			ERR_EXIT("shmid");
+		}
+		
+		fifo->p_shm = shmat(shmid, NULL, 0);
+		
+		//初始化shmhead
+		fifo->p_shm->blksize = blksz;
+		fifo->p_shm->blocks = blks;
+		fifo->p_shm->rd_index = 0;
+		fifo->p_shm->wr_index = 0;
+		
+		//初始化shmfifo
+		fifo->p_payload = (char*)(fifo->p_shm+1);
+		fifo->shmid = shmid;
+		fifo->sem_mutex = semget(key+1,1,IPC_CREAT|0644);
+		fifo->sem_full = semget(key+2,1,IPC_CREAT|0644);
+		fifo->sem_empty = semget(key+3,1,IPC_CREAT|0644);
+		
+		union semun su;
+		su.val = 1;
+		semctl(fifo->sem_mutex, 0, SETVAL, su);
+		su.val = 0;
+		semctl(fifo->sem_empty, 0, SETVAL, su);
+		su.val = blks;
+		semctl(fifo->sem_full, 0, SETVAL, su);
+
+	}else{ //打开共享内存成功
+	
+	}	
+}
+
+static int P(int shmid)
+{
+	struct sembuf buf[1];
+	buf[0].sem_num = 0;
+	buf[0].sem_op = -1;
+	buf[0].sem_flg = SEM_UNDO;
+	return semop(shmid, buf,1);
+}
+static void V(int shmid)
+{
+	struct sembuf buf[1];
+	buf[0].sem_num = 0;
+	buf[0].sem_op = 1;
+	buf[0].sem_flg = SEM_UNDO;
+	return semop(shmid, buf,1);
+}
+void shmfifo_put(shmfifo_t *fifo, const void *buf)
+{
+	P(fifo->sem_full);
+	P(fifo->sem_mutex);
+	//放入数据
+	memset(fifo->p_payload+fifo->p_shm->blksize*fifo->p_shm->wr_index,buf,fifo->shm->blksize);
+	fifo->p_shm->wr_index = (fifo->p_shm->wr_index + 1)%fifo->p_shm->blocks; 
+	V(fifo->sem_mutex);
+	V(fifo->sem_empty);
+}
+
+void shmfifo_get(shmfifo_t *fifo, void *buf)
+{
+	P(fifo->sem_empty);
+	P(fifo->sem_mutex);
+	//获取数据
+	memset(buf,fifo->p_payload+ fifo->p_shm->rd_index*fifo->p_shm->blksize,fifo->p_shm->blksize);
+	fifo->p_shm->rd_index = (fifo->p_shm->rd_index-1)%fifo->p_shm->blocks;
+	V(fifo->sem_mutex);
+	V(fifo->sem_full);
+
+}
+
+void shmfifo_destroy(shmfifo_t *fifo)
+{
+
+}
---
+++ shmfifo/shmfifo.h
@@ -0,0 +1,42 @@
+#ifndef _SHMFIFO_H_
+#define _SHMFIFO_H_
+
+#include<stdio.h>
+#include<sys/types.h>
+#include<sys/sem.h>
+#include<sys/ipc.h>
+#include<unistd.h>
+#include<stdlib.h>
+#include<string.h>
+#include<errno.h>
+
+
+#define ERR_EXIT(msg) \
+do{\
+	fprintf(stderr,"[%s][%d] %s : %d\n",__FILE__, __LINE__,msg,strerrno(errno));\
+	exit(EXIT_FAILURE);\
+}while(0)
+
+
+typedef struct shmhead {
+		unsigned int blksize;    // 块大小
+		unsigned int blocks;     // 块总数
+		unsigned int rd_index;   // 读索引
+		unsigned int wr_index;   // 写索引
+}shmhead_t;
+
+typedef struct shmfifo {
+		shmhead_t *p_shm; // 共享内存的头部地址 
+		char *p_payload;  // 有效负载起始地址
+		int shmid;        // 共享内存ID
+		int sem_mutex;    // 用来互斥的信号量
+		int sem_full;     // 用来控制共享内存满的信号量
+		int sem_empty;    // 用来控制共享内存空的信号量
+}shmfifo_t;
+
+shmfifo_t *shmfifo_init(key_t key, int blksz, int blks);
+void shmfifo_put(shmfifo_t *fifo, const void *buf);
+void shmfifo_get(shmfifo_t *fifo, void *buf);
+void shmfifo_destroy(shmfifo_t *fifo);
+
+#endif //_SHMFIFO_H_
